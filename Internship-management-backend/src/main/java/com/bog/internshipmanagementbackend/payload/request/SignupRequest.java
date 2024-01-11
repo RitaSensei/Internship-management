13d13a9b84a4564bad6bb8b6aed9718be9eb9fe3
Ghita <85077489+Ghita-L28@users.noreply.github.com>
2024-01-11 02:04:50 +0100

    updated authentication service

--- Internship-management-backend/src/main/java/com/bog/internshipmanagementbackend/payload/request/SignupRequest.java
+++ Internship-management-backend/src/main/java/com/bog/internshipmanagementbackend/payload/request/SignupRequest.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 import java.util.Set;
 
 @Setter
@@ -12,26 +15,26 @@ import java.util.Set;
 public class SignupRequest {
     @NotBlank
     private String nom;
-
     @NotBlank
     private String prenom;
-
-    //    @NotBlank
+    @NotBlank
     private String numPerso;
-
+    @NotBlank
+    private String adresse;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dateNaissance;
+    @NotBlank
+    private String sexe;
     @NotBlank
     @Size(min = 3, max = 20)
     private String username;
-
     @NotBlank
     @Size(max = 50)
     @Email
     private String email;
-
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
-
     @NotBlank
     private String role;
 }
